dd2cca625289bbd3a989abd18ee48e870f7aeccd
larry Caichihua <lcaichihua@hotmail.com>
2020-01-29 17:21:13 -0500

    add eureka server

---
+++ src/test/java/com/cavetech/springboot/EurekaServer/app/SpringbootEurekaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cavetech.springboot.EurekaServer.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootEurekaServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
